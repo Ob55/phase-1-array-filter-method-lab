2ed516504515b2fce00610df09166f99782eaa6e
Ob55 <brian55mwangi@gmail.com>
2023-12-18 23:11:29 +0300

    done assigment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const sinon = require('sinon');
+
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letters) {
+  const regex = new RegExp(`^${letters}`, 'i');
+  return drivers.filter(driver => regex.test(driver));
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
